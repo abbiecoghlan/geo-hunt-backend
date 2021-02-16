48653a690568db7a12e573d89a8608d11964045b
Marc <marc.ferraro1@gmail.com>
2021-02-16 17:19:44 -0600

    Added Show and Seed Update

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -5,4 +5,11 @@ class PuzzlesController < ApplicationController
 
         render json: puzzles
     end
+
+    def show
+        puzzle = Puzzle.find_by(id: params[:id])
+        
+        render json: puzzle
+    end
+
 end
