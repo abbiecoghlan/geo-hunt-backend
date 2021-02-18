fabfd9c8dcfdef24da73f0007c09d0e1ad7f9f8f
Marc <marc.ferraro1@gmail.com>
2021-02-17 19:58:36 -0600

    Change route to post

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :puzzles
   resources :users
   
-  get "/login", to: "users#login", as: "login"
+  post "/login", to: "users#login", as: "login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
